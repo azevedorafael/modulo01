92e7cfe925a4b8f41f377a34e1abfea156a1a436
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-08-19 22:43:41 -0300

    Add basic REST routes

--- index.js
+++ index.js
@@ -2,13 +2,46 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 const users = ['Rafael', 'Silva', 'Azevedo']
 
+server.get('/users', (req,res) => {
+    return res.json(users);
+})
+
 server.get('/users/:index', (req,res) => {
-    const {index}  = req.params
+    const {index}  = req.params;
 
     return res.json({message: `${users[index]}`});
+})
+
+server.post('/users', (req,res) => {
+    const {name} = req.body;
+
+    users.push(name);
+
+    return res.json(users);
+})
+
+server.put('/users/:index', (req,res) => {
+    const {index} =req.params;
+    const {name} =req.body;
+
+    users[index] = name;
+
+    return res.json(users);
 });
 
-server.listen(3000);
+server.delete('/users/:index', (req,res) => {
+    const {index} = req.params;
+
+    users.splice(index, 1);
+
+    return res.send();
+});
+
+server.listen('3000', () => {
+    console.log('server running')
+});
 
