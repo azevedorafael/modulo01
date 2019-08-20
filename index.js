4de3d83c3f63e279453e098a364f3a5022dc089f
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-08-19 22:22:54 -0300

    Add nodemon + script

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req,res) => {
-    return res.json({message: 'Hello World'});
+const users = ['Rafael', 'Silva', 'Azevedo']
+
+server.get('/users/:index', (req,res) => {
+    const {index}  = req.params
+
+    return res.json({message: `${users[index]}`});
 });
 
 server.listen(3000);
